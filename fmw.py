c291e04e98a13076e91903509e1869de10c3cb23
ponsfrilus <ponsfrilus@gmail.com>
2017-05-05 12:01:00 +0200

    crimList + crim telegram bot

--- fmw.py
+++ fmw.py
@@ -7,11 +7,27 @@ html_page = requests.get('https://www.fbi.gov/wanted/cyber')
 html_txt = html_page.text
 soup = BeautifulSoup(html_txt, 'html.parser')
 names = soup.findAll("div", { "class" : "focuspoint" })
-# print(names[0].attrs['data-base-url'])
-for criminal in names :
-    url_personne = criminal.attrs['data-base-url'].split('@@')[0]
-    name = criminal.img.attrs['alt']
-    print(name + " : " + url_personne)
 
+def getCriminal_name(criminal):
+    return(name)
+
+def getCriminal_detail(criminal):
+    url_personne = criminal.attrs['data-base-url'].split('@@')[0]
+    return(url_personne)
+
+def getCriminals():
+    return(names)
+
+def getCriminalsList():
+    criminals = {}
+    for criminal in names :
+        url_personne = criminal.attrs['data-base-url'].split('@@')[0]
+        name = criminal.img.attrs['alt']
+        criminals[name] = url_personne
+    return criminals    
+
+
+    #print(url_personne.split('@@')[0])
 #url_personne = names[0].attrs['data-base-url']
-#print(url_personne.split('@@')[0])
+if __name__ == '__main__':
+    print(getCriminals())
